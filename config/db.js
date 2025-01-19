ce773ac061f44638a2bcd74a24a93f51de715c96
paspi <paspigui@gmail.com>
2025-01-19 22:44:09 +0100

    identifiants bdd

--- config/db.js
+++ config/db.js
@@ -1,12 +1,11 @@
 const mysql = require("mysql")
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_NAME || "Exam",
-  socketPath:
-    process.env.DB_SOCKET_PATH || "/Applications/MAMP/tmp/mysql/mysql.sock",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  socketPath: process.env.DB_SOCKET_PATH,
 })
 
 connection.connect((err) => {
